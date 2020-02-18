e85dfa477c929e0ad4820524f3733d8ac6962876
Dolapo Olaniawo <43099885+Dolapola@users.noreply.github.com>
2020-02-18 18:46:46 -0500

    custom domain

--- Nine_app/settings.py
+++ Nine_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://ninefinanceapp.herokuapp.com/']
+ALLOWED_HOSTS = ['calm-honeydew-mbvnin85n5frbabpa43hd9sx.herokudns.com']
 
 
 # Application definition
