9031b75d3bb5961657c8e170157e4633b04a2fb5
Dolapo Olaniawo <43099885+Dolapola@users.noreply.github.com>
2020-02-21 16:09:20 -0500

    allowed host

--- Nine_app/settings.py
+++ Nine_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://ninefinanceapp.herokuapp.com/']
+ALLOWED_HOSTS = [' http://nine.finance']
 
 
 # Application definition
