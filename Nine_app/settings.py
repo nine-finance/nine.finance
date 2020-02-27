3eb2c1beccbeeed64bc8cacef895a2a8dcb6d535
Dolapo Olaniawo <43099885+Dolapola@users.noreply.github.com>
2020-02-27 12:15:05 -0500

    allowed hosts

--- Nine_app/settings.py
+++ Nine_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['http://nine.finance', 'www.nine.finance']
+ALLOWED_HOSTS = ['167.172.129.47']
 
 
 # Application definition
