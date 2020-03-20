6c901461de1e946d5909be2ac25f71a61213c7cb
Dolapo Olaniawo <43099885+Dolapola@users.noreply.github.com>
2020-03-20 13:52:54 -0400

    adding blog app (attempt 3)

--- Nine_app/urls.py
+++ Nine_app/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("app.urls")),
-    path('', include("blog.urls")),
+    path('blog/', include("blog.urls")),
     path('api-auth/', include('rest_framework.urls'))
 ]
 
--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from . import views
 from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 
 urlpatterns = [
-    path('blog/', PostListView.as_view(), name="blog-home"),
+    path('', PostListView.as_view(), name="blog-home"),
     path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
     path('post/new/', PostCreateView.as_view(), name="post-create"),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name="post-update"),
