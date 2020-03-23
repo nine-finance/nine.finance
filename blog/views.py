a4e7b933f2fe9c728b9d3eef9c555f1d6f780502
Dolapo Olaniawo <43099885+Dolapola@users.noreply.github.com>
2020-03-23 15:30:41 -0400

    configuring djangoadmin filepicker

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 from .models import post
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
 
 def blog(request):
     context = {
