f518c98442bdb00d6385dea0b67766578a889da0
Dolapo Olaniawo <43099885+Dolapola@users.noreply.github.com>
2020-03-29 17:57:57 -0400

    blog css

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.preloader, name="app-preloader"),
     path('home/', views.home, name="app-home"),
+    path('home/', views.PostListView.as_view(), name="blog-home"),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name="post-detail")
 ]
