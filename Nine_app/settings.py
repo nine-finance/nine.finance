fdb67225b7d34c8138da61155a7a97b831ac8012
Dolapo Olaniawo <43099885+Dolapola@users.noreply.github.com>
2020-03-31 17:43:21 -0400

    blog text field

--- Nine_app/settings.py
+++ Nine_app/settings.py
@@ -157,5 +157,5 @@ DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 django_heroku.settings(locals())
 
 CKEDITOR_UPLOAD_PATH = "blog_media/"
-AWS_QUERYSTRING_AUTH = False
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+
