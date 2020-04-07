3f0f9eb79a53bacb0721f19b5bc33ffca0120435
Dolapo Olaniawo <43099885+Dolapola@users.noreply.github.com>
2020-04-06 22:47:12 -0400

    additional plugins

--- app/models.py
+++ app/models.py
@@ -18,7 +18,7 @@ class timeline(models.Model):
 
 class post(models.Model):
     title=models.CharField(max_length=100)
-    content=RichTextUploadingField(null=True, blank=True, extra_plugins=['Embed', 'VideoDetector'])
+    content=RichTextUploadingField(null=True, blank=True)
     summary=models.TextField(null=True, blank=True)
     media = models.ImageField(default='background copy.png', upload_to='blog_media')
     date=models.DateTimeField(default=timezone.now)
