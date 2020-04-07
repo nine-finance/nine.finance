9f3ec222e36ff45366a209e5436112e642be13b7
Dolapo Olaniawo <43099885+Dolapola@users.noreply.github.com>
2020-04-06 20:06:42 -0400

    additional plugins

--- app/models.py
+++ app/models.py
@@ -18,8 +18,7 @@ class timeline(models.Model):
 
 class post(models.Model):
     title=models.CharField(max_length=100)
-    content=RichTextUploadingField(null=True, blank=True, external_plugin_resources=([ '/staticfiles/ckeditor/ckeditor/plugins/videodetector-master/'
-                                                                                        'plugin.js', ]),
+    content=RichTextUploadingField(null=True, blank=True,
                                    )
     summary=models.TextField(null=True, blank=True)
     media = models.ImageField(default='background copy.png', upload_to='blog_media')
