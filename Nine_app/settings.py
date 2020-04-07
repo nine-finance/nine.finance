8c37fd8e885a99d37eaff6e450b26f3cae081e03
Dolapo Olaniawo <43099885+Dolapola@users.noreply.github.com>
2020-04-06 23:59:18 -0400

    additional plugins

--- Nine_app/settings.py
+++ Nine_app/settings.py
@@ -167,11 +167,10 @@ CKEDITOR_CONFIGS = {
     },
     'special': {
         'toolbar': 'special',
-        'toolbar_special': [['Embed', 'AutoEmbed','Image', 'Flash', 'Table',],
+        'toolbar_special': [['Embed', 'Image', 'Flash', 'Table',],
                             ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-','JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
                             ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript'],
                             ['Styles', 'Format', 'Font', 'FontSize', 'TextColor', 'BGColor', 'Link', 'Unlink',]],
-        'extraPlugins': ','.join(['Embed', 'AutoEmbed'])
     }
 }
 
